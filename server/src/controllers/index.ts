fecdfec65ebfbcb03c63fa5292e353f50a981c82
Keyhan Mohammadi <105974026+mrcimiyagar@users.noreply.github.com>
2023-10-19 13:22:40 +0330

    fixed a typescript import bug in controllers adding lines.

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -5,11 +5,14 @@ import TowerController from "./tower.controller";
 import TowerService from "../services/tower.service";
 import RoomService from "../services/room.service";
 import RoomController from "./room.controller";
+import InviteController from "./invite.controller";
+import InviteService from "../services/invite.service";
 
 const build = () => {
     NetworkDriver.instance.registerController(HumanController, HumanService)
     NetworkDriver.instance.registerController(TowerController, TowerService)
     NetworkDriver.instance.registerController(RoomController, RoomService)
+    NetworkDriver.instance.registerController(InviteController, InviteService)
 }
 
 export {
