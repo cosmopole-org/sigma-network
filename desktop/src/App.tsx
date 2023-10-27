7b1f1ba2e03b7d92693b05704bf27dd4895d78e2
Keyhan Mohammadi <105974026+mrcimiyagar@users.noreply.github.com>
2023-10-27 23:27:25 +0330

    working on sigma-desktop module...

---
+++ desktop/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Desktop from './Desktop';
+
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Desktop />
+    </div>
+  );
+}
+
+export default App;
