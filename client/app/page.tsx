82a805cb940298c466a77a2fac8f8254997163a7
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-04-13 11:48:58 +0330

    fixed bug.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react";
 
 export default function Main() {
+	const router = useRouter();
+	useEffect(() => {
+		router.push('/app/home/spaces');
+	}, [])
 	return (
-		<div className="w-full h-full">
-			Welcome to Sigma!
-		</div>
+		<div className="w-full h-full" />
 	)
 }
