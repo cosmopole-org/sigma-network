e8fc8fde23e1f45c4846ebf37d522bbf5846c9d6
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-04-13 18:17:18 +0330

    migrated human service to grpc.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Card, CircularProgress } from "@nextui-org/react";
 import { useRouter } from "next/navigation"
 import { useEffect } from "react";
 
@@ -9,6 +10,12 @@ export default function Main() {
 		router.push('/app/home/spaces');
 	}, [])
 	return (
-		<div className="w-full h-full" />
+		<div className="w-full h-full">
+			<Card shadow="none" radius="none" className="w-full h-full fixed left-0 top-0" style={{ zIndex: 50 }}>
+				<Card className="w-24 h-24 fixed left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 items-center justify-center">
+					<CircularProgress />
+				</Card>
+			</Card>
+		</div>
 	)
 }
