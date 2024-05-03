961cf2575986971dba936b2fb520ef5ec4f8a398
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-05-03 18:30:22 +0330

    test api service.

--- servers/newmain/main.go
+++ servers/newmain/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"os"
 	app "sigma/main/core/core"
-	//"sigma/main/core/dtos"
+	"sigma/main/core/dtos"
+
 	"sigma/main/core/modules"
 	"strconv"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
-func hello(app *modules.App, d interface{}, assistant modules.Assistant) (any, error) {
+func hello(app *modules.App, d dtos.HelloDto, assistant modules.Assistant) (any, error) {
 	return `{ "hello": "world" }`, nil
 }
 
@@ -24,36 +26,37 @@ func main() {
 	}
 
 	app := app.New(
-	    os.Getenv("PORT"),
+		os.Getenv("PORT"),
 		os.Getenv("POSTGRES_URI"),
-		os.Getenv("POSTGRES_DB") + "_" + os.Getenv("PORT"),
+		os.Getenv("POSTGRES_DB")+"_"+os.Getenv("PORT"),
 		os.Getenv("REDIS_URI"),
 		os.Getenv("STORAGE_ROOT_PATH"),
 		os.Getenv("MAP_SERVER"),
 	)
 
-	//var apiService = modules.CreateService(app, "api")
-	// apiService.AddMethod(
-	// 	modules.CreateMethod(
-	// 		"hello",
-	// 		hello,
-	// 		modules.CreateCheck(false, false, false),
-	// 		dtos.HelloDto{},
-	// 		modules.CreateMethodOptions(true, true, false),
-	// 	),
-	// )
-	// apiService.AddMethod(
-	// 	modules.CreateMethod(
-	// 		"ping",
-	// 		func(app *modules.App, d interface{}, assistant modules.Assistant) (any, error) {
-	// 			return os.Getenv("PORT"), nil
-	// 		},
-	// 		modules.CreateCheck(false, false, false),
-	// 		dtos.PingDto{},
-	// 		modules.CreateMethodOptions(true, true, false),
-	// 	),
-	// )
-	//app.AddService(apiService)
+	modules.AddMethod[dtos.HelloDto, dtos.HelloDto](
+		app,
+		modules.CreateMethod[dtos.HelloDto, dtos.HelloDto](
+			"/api/hello",
+			hello,
+			dtos.HelloDto{},
+			modules.CreateCheck(false, false, false),
+			modules.CreateMethodOptions(true, fiber.MethodGet, true, false),
+		),
+	)
+
+	modules.AddMethod[dtos.PingDto, dtos.PingDto](
+		app,
+		modules.CreateMethod[dtos.PingDto, dtos.PingDto](
+			"/api/ping",
+			func(app *modules.App, d dtos.PingDto, assistant modules.Assistant) (any, error) {
+				return os.Getenv("PORT"), nil
+			},
+			dtos.PingDto{},
+			modules.CreateCheck(false, false, false),
+			modules.CreateMethodOptions(true, fiber.MethodGet, true, false),
+		),
+	)
 
 	var portStr = os.Getenv("PORT")
 	port, err := strconv.ParseInt(portStr, 10, 32)
@@ -61,7 +64,7 @@ func main() {
 		panic(err)
 	}
 
-	app.Network.Listen(modules.CreateListenOptions(true, int(port), true, int(port + 2)))
+	app.Network.Listen(modules.CreateListenOptions(true, int(port), true, int(port+2)))
 
 	<-quit
 }
