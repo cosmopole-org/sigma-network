61114bcfffb8e8733b0eb41d4a5771266014f24c
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-05-19 22:38:07 +0330

    found the best way in sigma lab.

--- tests/rest/stress/main.go
+++ tests/rest/stress/main.go
@@ -1,106 +1,53 @@
 package main
 
-import (
-	"bytes"
-	"errors"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"sync"
-)
+import "fmt"
 
-const serverUrl = "https://midopia-football.liara.run"
-
-type Res struct {
-	Message string `json:"message"`
+type Hello struct {
+	Content1 string
 }
 
-func hello(i int, url string, client *http.Client, token string, body []byte, wg *sync.WaitGroup) {
-	defer wg.Done()
-	r, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	r.Close = true
-	r.Header.Add("Content-Type", "application/json")
-	r.Header.Add("token", token)
-	res, err := client.Do(r)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer res.Body.Close()
-	result, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(string(result))
-	if res.StatusCode != http.StatusOK {
-		log.Println(errors.New("request failed"))
-	}
+func (h *Hello) GetContent() string {
+	return h.Content1
 }
 
-var body3 = []byte(`{}`)
+type Bye struct {
+	Content2 string
+}
 
-func hello2(i int, client *http.Client, wg *sync.WaitGroup) {
-	defer wg.Done()
-	r, err := http.NewRequest("GET", serverUrl+"/api/hello", bytes.NewBuffer(body3))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	r.Close = true
-	res, err := client.Do(r)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer res.Body.Close()
-	result, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(string(result))
-	if res.StatusCode != http.StatusOK {
-		log.Println(errors.New("request failed"))
-	}
+func (b *Bye) GetContent() string {
+	return b.Content2
+}
+
+type Packet interface {
+	*Hello | *Bye
+	GetContent() string
+}
+
+func ExtractPacket[T Packet](x T) string {
+	return x.GetContent()
+}
+
+type Action struct {
+	Process func(interface{})
+}
+
+type Services struct {
+	Actions map[string]Action
+}
+
+func hello(c Hello) {
+	fmt.Println(c.GetContent())
+}
+
+func (ss *Services) RunAction(key string, input interface{}) {
+	ss.Actions["hello"].Process(input)
 }
 
 func main() {
-	// body := []byte(`{
-	// 	"leagueLevel": 2
-	// }`)
-
-	// body2 := []byte(`{
-	// 	"result": "win",
-	// 	"goalP": 10,
-	// 	"goalN": 0,
-	// 	"leagueLevel": 2
-	// }`)
-
-	// client := &http.Client{
-	// 	Timeout:   500 * time.Second,
-	// 	Transport: &http.Transport{},
-	// }
-
-	// var wg sync.WaitGroup
-
-	// println("started test.")
-
-	// start := time.Now()
-
-	// for i := 0; i < 1000; i++ {
-	// 	wg.Add(1)
-	// 	go hello2(i, client, &wg)
-	// 	wg.Add(1)
-	// 	go hello(i, serverUrl+"/players/readLeaderBoard", client, "SbZrFS4nnXWwYHX5y9mKiKTvvZuhoUan", body, &wg)
-	// 	wg.Add(1)
-	// 	go hello(i, serverUrl+"/players/submitScore", client, "SbZrFS4nnXWwYHX5y9mKiKTvvZuhoUan", body2, &wg)
-	// }
-	// wg.Wait()
-	// elapsed := time.Since(start)
-	// log.Println("Binomial took ", elapsed)
+	services := Services{Actions: map[string]Action{}}
+	action1 := Action{Process: func(raw interface{}) {
+		hello(raw.(Hello))
+	}}
+	services.Actions["hello"] = action1
+	services.RunAction("hello", Hello{Content1: "test world !"})
 }
