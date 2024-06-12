da4302fe3ec813473c472a3757f143b059740425
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-06-13 01:13:47 +0330

    fixed bugs. crypto beta ready !

--- sigma/client/src/security.ts
+++ sigma/client/src/security.ts
@@ -1,10 +1,21 @@
 
-import CryptoJS from 'crypto-js';
+type PublicKeyBatch = { [id: string]: CryptoKey };
+
+type RefreshEvents = { [id: string]: string };
+
+type GroupKey = { keyId: string, key: CryptoKey }
+
+type Group = {
+    groupId: string
+    key: GroupKey,
+    publicKeys: PublicKeyBatch,
+    history: RefreshEvents
+};
 
 export default class Security {
+    private groups: {[id: string]: Group} = {}
     private myKeyPair?: CryptoKeyPair
-    private publicKeys: {[id: string]:CryptoKey} = {}
-    private keySingles: {[id: string]: string} = {}
+    private keyStore: {[id: string]: CryptoKey} = {}
     public async getMyKeyPair() : Promise<CryptoKeyPair> {
         if (!this.myKeyPair) {
             this.myKeyPair = await window.crypto.subtle.generateKey(
@@ -17,41 +28,78 @@ export default class Security {
                 true,
                 ["encrypt", "decrypt"],
             );
-            this.publicKeys["me"] = this.myKeyPair.publicKey
         }
         return this.myKeyPair;
     }
-    public getOrGenerateSingleKey(keyId: string) : string {
-        if (!this.keySingles[keyId]) {
-            this.keySingles[keyId] = Array.from(
-                window.crypto.getRandomValues(new Uint8Array(Math.ceil(32 / 2))),
-                (b) => ("0" + (b & 0xFF).toString(16)).slice(-2)
-            ).join("");
+    public async encryptGroupPacket(identifier: string, groupId: string, data: string): Promise<string> {
+        this.groups[groupId].history[identifier] = this.groups[groupId].key.keyId;
+        return await this.encryptDataByKey(this.groups[groupId].key.key, data)
+    }
+    public async decryptGroupPacket(identifier: string, groupId: string, cipher: string): Promise<string> {
+        this.groups[groupId].history[identifier] = this.groups[groupId].key.keyId;
+        return await this.decryptDataByKey(this.groups[groupId].key.key, cipher);
+    }
+    public async decryptGroupKey(keyCipher: string): Promise<string> {
+        return await this.decryptKey(keyCipher);
+    }
+    public async refreshGroupKey(groupId: string): Promise<{ [id: string]: string }> {
+        let group = this.groups[groupId];
+        let { id, key } = await this.generateNewKey();
+        group.key = {keyId: id, key: key};
+        return this.encryptKeyBatch(group.publicKeys, group.key.key);
+    }
+    public async newGroup(groupId: string): Promise<void> {
+        let {id, key} = await this.generateNewKey();
+        await this.putGroup({
+            groupId: groupId,
+            key: {key: key, keyId: id},
+            publicKeys: {},
+            history: {}
+        });
+    }
+    public deleteGroup(groupId: string) {
+        delete this.groups[groupId];
+    }
+    public async putGroup(group: {groupId: string, key: GroupKey, publicKeys: {[id: string]: string}, history: RefreshEvents}): Promise<void> {
+        let g: Group = {
+            groupId: group.groupId,
+            key: group.key,
+            publicKeys: {},
+            history: group.history
+        };
+        let ids = Object.keys(group.publicKeys);
+        for (let i = 0; i < ids.length; i++) {
+            g.publicKeys[ids[i]] = await this.parsePublicKey(group.publicKeys[ids[i]]);
         }
-        return this.keySingles[keyId];
+        if (!this.groups[group.groupId]) {
+            this.groups[group.groupId] = g
+        }
     }
-    public getSingleKey(keyId: string) : string {
-        return this.keySingles[keyId]
+
+    // internal api
+    private async generateNewKey() : Promise<{id: string, key: CryptoKey}> {
+        let id = window.crypto.randomUUID().toString();
+        this.keyStore[id] = await window.crypto.subtle.generateKey(
+           { name: "AES-GCM", length: 256 },
+            true,
+            ["encrypt", "decrypt"],
+        );
+        return {id: id, key: this.keyStore[id]};
     }
-    public putSingleKey(keyId: string, key: string) : void {
-        this.keySingles[keyId] = key;
+    private async parsePublicKey(publicKey: string) : Promise<CryptoKey> {
+        return await window.crypto.subtle.importKey("spki", new Uint8Array(this.b642ab(publicKey)), { name: "RSA-OAEP", hash: "SHA-256" }, false, [ "encrypt" ]);
     }
-    public async encryptKeyByPublicKey(keyId: string, key: string): Promise<string> {
+    private async encryptKey(publicKey: CryptoKey, key: string): Promise<string> {
         let encodedKey = new TextEncoder().encode(key);
-        return this.bufferKeyToHexStr(await window.crypto.subtle.encrypt(
+        return this.ab2b64(await window.crypto.subtle.encrypt(
             {
                 name: "RSA-OAEP",
             },
-            this.publicKeys[keyId],
+            publicKey,
             encodedKey
         ));
     }
-    public async decryptAndStoreKey(keyId: string, cipherKey: string): Promise<string> {
-        let key = await this.decryptKey(cipherKey);
-        this.putSingleKey(keyId, key);
-        return key;
-    }
-    public async decryptKey(cipherKey: string): Promise<string> {
+    private async decryptKey(cipherKey: string): Promise<string> {
         const dec = new TextDecoder();
         return dec.decode(await window.crypto.subtle.decrypt(
             {
@@ -61,41 +109,42 @@ export default class Security {
             this.b642ab(cipherKey)
         ));
     }
-    public async encryptKeyBatch(keyIds: string[], key: string): Promise<{[id: string]: string}> {
-        let encodedKey = new TextEncoder().encode(key);
+    private async encryptKeyBatch(keys: PublicKeyBatch, key: CryptoKey): Promise<{[id: string]: string}> {
+        let exportedKey = await crypto.subtle.exportKey('raw', key);
         let keyMap: {[id: string]: string} = {};
-        for (let i = 0; i < keyIds.length; i++) {
-            keyMap[keyIds[i]] = this.ab2b64(await window.crypto.subtle.encrypt(
+        let ids = Object.keys(keys);
+        for (let i = 0; i < ids.length; i++) {
+            keyMap[ids[i]] = this.ab2b64(await window.crypto.subtle.encrypt(
                 {
                     name: "RSA-OAEP",
                 },
-                this.publicKeys[keyIds[i]], // from generateKey or importKey above
-                encodedKey // ArrayBuffer of data you want to encrypt
+                keys[ids[i]], // from generateKey or importKey above
+                exportedKey // ArrayBuffer of data you want to encrypt
             ))
         }
         return keyMap;
     }
-    public async encryptDataByKey(keyId: string, data: string): Promise<string> {
-        return CryptoJS.AES.encrypt(
-            data,
-            this.keySingles[keyId]
-        ).toString();
+    private async encryptDataByKey(key: CryptoKey, data: string): Promise<string> {
+        let enc = new TextEncoder();
+        let iv = crypto.getRandomValues(new Uint8Array(12));
+        let ivStr = Array.from(iv).map(b => String.fromCharCode(b)).join('');
+        let encrypted = await window.crypto.subtle.encrypt(
+            { name: "AES-GCM", iv: iv },
+            key,
+            enc.encode(data)
+        );
+        const ctArray = Array.from(new Uint8Array(encrypted));
+        const ctStr = ctArray.map(byte => String.fromCharCode(byte)).join('');
+        return btoa(ivStr+ctStr);
     }
-    public async decryptDataByKey(keyId: string, cipher: string): Promise<string> {
-        return CryptoJS.AES.decrypt(
-            cipher,
-            this.keySingles[keyId]
-        ).toString(CryptoJS.enc.Utf8);
+    private async decryptDataByKey(key: CryptoKey, cipher: string): Promise<string> {
+        const ivStr = atob(cipher).slice(0,12);
+        const iv = new Uint8Array(Array.from(ivStr).map(ch => ch.charCodeAt(0)));
+        const ctStr = atob(cipher).slice(12);
+        const ctUint8 = new Uint8Array(Array.from(ctStr).map(ch => ch.charCodeAt(0)));
+        const plainBuffer = await window.crypto.subtle.decrypt({ name: 'AES-GCM', iv: iv }, key, ctUint8);
+        return new TextDecoder().decode(plainBuffer);
     }
-    public async addRecipientKey(userId: string, publicKey: string) {
-        this.publicKeys[userId] = await window.crypto.subtle.importKey("spki",
-            new Uint8Array(this.b642ab(publicKey)),
-            {name: "RSA-OAEP", hash: "SHA-256"},
-            false,
-            ["encrypt"]
-        )
-    }
-    // internal functions
     private b642ab(base64string: string) : ArrayBuffer{
         return Uint8Array.from(atob(base64string), c => c.charCodeAt(0)).buffer;
     }
@@ -106,8 +155,5 @@ export default class Security {
         return Array.from(buffer)
             .map(byte => byte.toString(16).padStart(2, '0'))
             .join('');
-    }
-    private bufferKeyToHexStr(buffer: ArrayBuffer) {
-        return this.toHex(new Uint8Array(buffer));
     }
 }
--- sigma/client/src/test.ts
+++ sigma/client/src/test.ts
@@ -6,24 +6,15 @@ import Security from './security';
 
     const message = "hello world !";
 
-    console.log(message)
-    security.getOrGenerateSingleKey("first key")
-    const cipher = await security.encryptDataByKey("first key", message);
-    console.log(cipher)
-
     await security.getMyKeyPair();
 
-    const sampleCount = 50000;
-
-    console.log("done 1 !");
-
-    let keyMap = await security.encryptKeyBatch(["me"], security.getOrGenerateSingleKey("first key"))
-
-    console.log(keyMap);
-
-    await security.decryptAndStoreKey("my group", keyMap["me"]);
-    const messageBack = await security.decryptDataByKey("my group", cipher);
+    console.log(message)
+    await security.newGroup("rasht");
+    console.log("start")
+    const cipher = await security.encryptGroupPacket("1","rasht", message);
+    console.log(cipher)
+    let messageBack = await security.decryptGroupPacket("2","rasht", cipher);
     console.log(messageBack)
 
-    console.log("done 2");
+    console.log("end");
 })();
