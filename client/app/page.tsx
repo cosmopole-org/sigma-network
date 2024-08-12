b36363864d103cf86eab2dfc2612bc09596bec2a
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-08-12 19:42:11 +0330

    fixing build bug.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,13 +4,13 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function Main() {
-	const router = useRouter();
-	useEffect(() => {
-		router.replace('/app/home/main');
-	}, []);
+	// const router = useRouter();
+	// useEffect(() => {
+	// 	router.replace('/app/home/main');
+	// }, []);
 	return (
 		<div className="w-full h-full">
-			
+			hello world !
 		</div>
 	)
 }
