c6d77158996dc48d5d2d038a4e55a3f53be8339f
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-12-25 13:47:16 +0330

    fixed bug.

--- safezone/index.js
+++ safezone/index.js
@@ -1,8 +1,16 @@
 
 const express = require('express')
+const https = require('https');
+const fs = require('fs');
 
 const app = express()
 app.use(express.static('files'))
-app.listen(3001, () => {
-    console.log('server started at port 3001')
-})
+
+const httpsServer = https.createServer({
+  key: fs.readFileSync('cert.key'),
+  cert: fs.readFileSync('cert.pem'),
+}, app);
+
+httpsServer.listen(8443, () => {
+    console.log('HTTPS Server running on port 443');
+});
