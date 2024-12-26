470f9ef8beda7148edceb243adc824473b48a056
Keyhan Mohammadi <hawkcosmopolitan@gmail.com>
2024-12-26 17:57:01 +0330

    fixed bug.

--- connect/src/components/elements/textfield.tsx
+++ connect/src/components/elements/textfield.tsx
@@ -1,15 +1,20 @@
 import { Input } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function TextField({ label, onChange, text }: Readonly<{ text?: string, label?: string, onChange?: (text: string) => void }>) {
+    const [t, setT] = useState(text ?? "");
     return (
         <Input
-            onChange={e => onChange && onChange(e.target.value)}
+            onChange={e => {
+                setT(e.target.value);
+                onChange && onChange(e.target.value);
+            }}
             classNames={{
                 inputWrapper: "w-full mt-3 h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
                 mainWrapper: "w-full",
                 base: "w-full"
             }}
-            value={text ?? ""}
+            value={t}
             label={label}
             size="lg"
         />
